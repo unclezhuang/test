20269bcaff98eac809c4747afb339b6042803d66
fengzi <3264893039@qq.com>
2023-05-22 10:48:01 +0800

    4

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,6 @@ declare module '@vue/runtime-core' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
     ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElFooter: typeof import('element-plus/es')['ElFooter']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElMain: typeof import('element-plus/es')['ElMain']
