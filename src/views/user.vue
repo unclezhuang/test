16068789c6fcb9fdfd99249224e5622c3344e9ef
陈姝彤 <1958500043@qq.com>
2023-06-03 19:09:03 +0800

    个人界面基本修改完毕

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="persion">
     <profile-card
       :avatar="avatar"
@@ -22,23 +22,23 @@
       <my-form ref="myFormRef"></my-form>
     <el-button class="write" type="success" @click="showForm">写帖子</el-button>
       <h2>历史发布的帖子</h2>
-    </div>
+    </div> -->
 
     <!-- 历史发布的帖子 -->
-    <div class="history-post">
-      <!-- 如果用户没有发布过帖子则显示提示信息 -->
-      <div class="no-post" v-if="!hasPost">还没有发过帖子</div>
+    <!-- <div class="history-post">
+     
+      <div class="no-post" v-if="!hasPost">还没有发过帖子</div> 
 
-      <!-- 如果用户已经发布过帖子则显示帖子列表 -->
+     
       <ul v-else>
-        <!-- 根据 v-for 循环遍历每个帖子并渲染 -->
+       
         <li v-for="post in postList" :key="post.id">{{ post.title }}</li>
       </ul>
     </div>
   </div>
-</template>
+</template> -->
 
-<script>
+<!-- <script>
 import { ref } from 'vue';
 import ProfileCard from "./ProfileCard.vue";
 import EditProfileForm from "./EditProfileForm.vue";
@@ -110,9 +110,9 @@ export default {
     };
   },
 };
-</script>
+</script> -->
 
-<style>
+<!-- <style>
 .persion{
   width: 33.33%;
   float: left;
@@ -151,4 +151,204 @@ export default {
   margin-top: 20px;
 }
 
+</style> -->
+
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside class="left">
+        <div class="avatar">
+      <img src="../img/少女熊猫.jpg" alt="image description" style=" width: 135px; height: 135px; margin-left:15%; margin-top:20%; border-radius: 50%; border: 5px solid yellow; box-sizing: border-box;">
+      <button class="profile-button">编辑个人资料</button>
+      <button class="post-button">写帖子</button>
+    </div>
+        </el-aside>
+      <el-container>
+        <el-header class="top">
+          <div class="info1">
+            <div class="left-side">
+        <span>name:Lihua</span>
+        <span>gender:male</span>
+        <span>email:Lihua</span>
+        <span>age:20</span>
+        </div>
+        <div class="center">
+        <span>博客：</span>
+        <span>粉丝数：</span>
+      </div>
+      <div class="right-side">
+        <span>note:</span>
+      </div>
+  </div>
+        </el-header>
+        <el-main class="main"><span class="bold">历史发布的帖子:</span>
+          <div class="post-group">
+            <div class="left-post-wrapper">
+    <button class="left-post"></button>
+    <div class="left-post-description">帖子一：人工智能产生的影响</div>
+  </div>
+  <div class="middle-post-wrapper">
+    <button class="middle-post"></button>
+    <div class="middle-post-description">帖子二：区块链的应用</div>
+  </div>
+  <div class="right-post-wrapper">
+    <button class="right-post"></button>
+    <div class="right-post-description">帖子三：点我看社区治理</div>
+  </div>
+  </div>
+        
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style>
+.top{
+  border: 1px solid grey;
+  height:35%;
+  
+}
+.left{
+  border: 1px solid grey;
+  width:200px; 
+  background-color:rgb(167, 164, 164);
+}
+.main{
+  border: 1px solid grey;
+}
+.info1{
+  display: flex;
+  justify-content:space-between;  /* 将姓名和性别信息向右对齐 */
+  /* align-items: center; */
+  height: 100%;
+  
+}
+.left-side{
+  display: flex;
+  flex-direction: column;
+
+}
+.center{
+  display: flex;
+  /* justify-content: center; */
+  /* align-items: center; */
+  flex-direction: column;
+  /* margin-top:3%; */
+}
+.right-side{
+  display: flex;
+  justify-content: flex-end;
+  margin-right:11rem;
+}
+.info1 .left-side > span {
+  margin-top: 22px;
+}
+.info1 .center > span {
+  margin-top:30px;
+}
+.info1 .right-side> span {
+  margin-top:25px;
+}
+.bold {
+  font-weight: bold;
+  font-size:large;
+  margin-left:1.5rem;
+  margin-top:1rem;
+}
+.profile-button {
+  display: block;
+  margin-top: 50px;
+  padding: 10px 20px;
+  border: none;
+  width:100%;
+  border-radius: 5px;
+  color: white;
+  font-size: 16px;
+  background-color:rgb(167, 164, 164);
+}
+.profile-button:hover {
+  background-color: rgba(210, 218, 223, 0.4);
+}
+.post-button{
+  display: block;
+  margin-top: 40px;
+  padding: 10px 20px;
+  border: none;
+  width:100%;
+  border-radius: 5px;
+  color: white;
+  font-size: 16px;
+  background-color:rgb(167, 164, 164);
+}
+.post-button:hover {
+  background-color: rgba(210, 218, 223, 0.4);
+}
+
+.main{
+  background-color:rgb(241, 184, 90);
+}
+
+.post-group {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 20px;
+}
+.left-post{
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+  border: none;
+  font-size: 14px;
+  margin-top: 4rem;
+  background-image: url("../img/pic1.png");
+  background-size: cover;
+  background-position: center;
+  box-shadow: 3px 3px 7px rgba(22, 22, 22, 0.5); /* 阴影 */
+}
+
+.middle-post{
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+  border: none;
+  font-size: 14px;
+  margin-top: 4rem;
+  background-image: url("../img/dog.png");
+  background-size: cover;
+  background-position: center;
+  box-shadow: 3px 3px 7px rgba(22, 22, 22, 0.5);
+}
+
+.right-post{
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+  border: none;
+  font-size: 14px;
+  margin-top: 4rem;
+  background-image: url("../img/pudding.png");
+  background-size: cover;
+  background-position: center;
+  box-shadow: 3px 3px 7px rgba(22, 22, 22, 0.5);
+}
+.left-post-wrapper,
+.middle-post-wrapper,
+.right-post-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.left-post-description,
+.middle-post-description,
+.right-post-description {
+  font-family: Arial, sans-serif;
+  font-size:10px;
+  margin-top: 10px;
+  margin-bottom: 20px; /* 下方留白 */
+}
 </style>
